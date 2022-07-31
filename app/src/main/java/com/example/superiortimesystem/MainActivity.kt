7bbcbc640e65abd492f3c46191f019a8df3b7178
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-07-30 22:23:22 -0400

    gets time

--- app/src/main/java/com/example/superiortimesystem/MainActivity.kt
+++ app/src/main/java/com/example/superiortimesystem/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.superiortimesystem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val timeText: TextView = findViewById(R.id.date_and_time) as TextView;
+        val updateTime: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    while (!this.isInterrupted) {
+                        sleep(1000)
+                        runOnUiThread {
+                            timeText.text = Calendar.getInstance().timeInMillis.toString();
+                            // TODO: display as decimal time and IFC
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                }
+            }
+        }
+
+        updateTime.start()
     }
+
 }
