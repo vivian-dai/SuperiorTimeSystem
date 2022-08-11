5de7f93a8c1289eb1b419a6783c3e7e984e6beed
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-08-11 19:07:44 -0400

    finished ifc widget

--- app/src/main/java/com/example/superiortimesystem/InternationalFixedCalendarDate.kt
+++ app/src/main/java/com/example/superiortimesystem/InternationalFixedCalendarDate.kt
@@ -9,7 +9,7 @@ import com.example.superiortimesystem.datetime.DateTime
 /**
  * Implementation of App Widget functionality.
  */
-class DecimalTime : AppWidgetProvider() {
+class InternationalFixedCalendarDate : AppWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -17,7 +17,7 @@ class DecimalTime : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateDecimalTimeAppWidget(context, appWidgetManager, appWidgetId)
+            updateIFCAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
@@ -30,14 +30,14 @@ class DecimalTime : AppWidgetProvider() {
     }
 }
 
-internal fun updateDecimalTimeAppWidget(
+internal fun updateIFCAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
     // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.decimal_time)
-    views.setTextViewText(R.id.appwidget_text_dt, DateTime().decimalTime())
+    val views = RemoteViews(context.packageName, R.layout.international_fixed_calendar_date)
+    views.setTextViewText(R.id.appwidget_text_ifc, DateTime().ifcDate())
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
